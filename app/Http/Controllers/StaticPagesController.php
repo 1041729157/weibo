80748e6b1c6c5c125778e4b26aa79d0698ab681a
HH <1041729157@qq.com>
2020-07-10 12:15:02 +0000

    基础页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //extends 是继承某个类,继承之后可以使用父类的方法,也可以重写父类的方法
+ 	public function home(){
+ 		return view('static_pages/home');
+ 		//渲染在 resources/views 文件夹下的 static_pages/home.blade.php 文件
+ 	}
+
+ 	public function help(){
+ 		return view('static_pages/help');
+ 	}
+
+ 	public function about(){
+ 		return view('static_pages/about');
+ 	}
+}
