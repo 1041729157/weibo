9a2e315066a3b501060a0adcd7dd1641b019de9c
HH <1041729157@qq.com>
2020-07-10 16:17:09 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+	<a href="https://learnku.com/laravel/courses" target="_blank"> 
+	<!-- target="_blank"--在新窗口中打开链接(html5属性)  -->
+		刻意练习，每日精进
+	</a>
+
+	<div class="float-right"><!-- 右浮动 -->
+		<a href="/about">关于</a>
+	</div>
+</footer>
