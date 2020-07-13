c99e21a368040d1c92e3fe3b83e4678dbbce959e
HH <1041729157@qq.com>
2020-07-13 12:37:12 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,7 +1,34 @@
 @extends('layouts.default')
 
+@section('title', '注册')
+
 @section('content')
-	<div >
-		<h1>注册</h1>
+	<div class="offset-md-2 col-md-8">
+		<div class="card">
+			<div class="card-header"><!-- 卡片按钮效果 -->
+				<h5>注册</h5>
+			</div>
+			<div class="card-body">
+				<form method="POST" action="{{ route('users.store') }}">
+					<div class="form-group">
+						<label for="name">名称：</label>
+						<input type="text" name="name" class="form-control" value="{{ old('name') }}"><!-- {{ old('name') }}页面进行重定向访问时，输入框将自动填写上最后一次输入过的数据 -->
+					</div>
+					<div class="form-group">
+						<label for="email">邮箱：</label>
+						<input type="text" name="email" class="form-control" value="{{ old('email') }}">
+					</div>
+					<div class="form-group">
+						<label for="password">密码：</label>
+						<input type="text" name="password" class="form-control" value="{{ old('password') }}">
+					</div>
+					<div class="form-group">
+						<label for="password_confirmation"></label>
+						<input type="txet" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+					</div>
+					<button type="submit" class="btn btn-primary">注册</button>
+				</form>
+			</div>
+		</div>
 	</div>
 @stop
