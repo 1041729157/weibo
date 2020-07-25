5b09c5c4de66816e9e6e9e1ddbedbd9febcaccbe
HH <1041729157@qq.com>
2020-07-25 13:56:23 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -2,9 +2,17 @@
 	<a href="{{ route('users.show', $user->id) }}">
 		<img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar">
 	</a>
-	<div class="midia-body">
+	<div class="media-body">
 		<!-- diffForHumans()日期进行友好化处理 -->
 		<h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small> </h5>
+
 		{{ $status->content }}
 	</div>
+	@can('destroy', $status)
+	<form action="{{ route('statuses.destroy', $status->id) }}" method="post" onsubmit="return confirm('您确定删除本条微博吗？')">
+		@csrf
+		@method('DELETE')
+		<button type="submit" class="btn btn-sm btn-danger">删除</button>
+	</form>
+	@endcan
 </li>
