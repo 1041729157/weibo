3b56d54950e70978b221cca47cd3e7e135269ef0
HH <1041729157@qq.com>
2020-07-26 14:47:03 +0000

    关注用户动态流

--- app/User.php
+++ app/User.php
@@ -56,7 +56,14 @@ class User extends Authenticatable
     }
 
     public function feed(){
-        return $this->statuses()->orderBy('created_at', 'desc');
+
+        // return $this->statuses()->orderBy('created_at', 'desc');
+
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);//将当前用户的 id 加入到 user_ids 数组中
+        //with 方法，预加载避免了 N+1 查找的问题
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
+        
     }
 
 
